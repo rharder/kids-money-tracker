a8042703b86af247e8b4f73970ca3c5605d348a1
Robert Harder <robertharder@gmail.com>
2026-08-26 11:36:01 -0600

    Use one child per mobile row

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "family-money-v5";
+const CACHE = "family-money-v6";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
